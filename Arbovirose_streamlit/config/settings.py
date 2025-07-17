e6e3d5b5b2352d5600c9370a29d79834b05272f6
Janice Gonzaga de Caralho Silva <janice.gonzaga@ufvjm.edu.br>
2025-07-16 21:14:13 -0300

    Update settings.py

--- Arbovirose_streamlit/config/settings.py
+++ Arbovirose_streamlit/config/settings.py
@@ -1,13 +1,13 @@
 import os
-from pathlib import Path
 
-# Identifica se está no Render
+# Verifica se estamos no Render
 IS_RENDER = os.getenv('RENDER', 'False').lower() == 'true'
 
 # Configuração do banco de dados
 if IS_RENDER:
-    DATABASE_URL = os.getenv('DATABASE_URL')
+    # Usará a variável de ambiente configurada em run_app.py
+    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///data/arbovirose.db")
 else:
-    # Desenvolvimento local
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    DATABASE_URL = f"sqlite:///{BASE_DIR}/arbovirose.db"
+    # Configuração local
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'data', 'arbovirose.db')}"
